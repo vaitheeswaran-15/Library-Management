1ab517a4e000c16fbf9f7f486c1d274d9b351d0a
Inbarasu <inbarasu@mallow-tech.com>
2023-03-09 23:40:23 +0530

    comments model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories do
-    resources :books
+    resources :books do
+      resources :comments, only: :create
+    end
   end
 end
