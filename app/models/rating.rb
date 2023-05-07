3d35d02f2fbc7dbff61ddc6ac77e2d40093cd815
Inbarasu <inbarasu@mallow-tech.com>
2023-05-07 13:25:42 +0530

    removed callbacks and counter cache

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,10 +1,3 @@
 class Rating < ApplicationRecord
-  belongs_to :paper, counter_cache: true
-
-  after_save :update_average_rating_for_paper
-
-  def update_average_rating_for_paper
-    avg_rating = paper.ratings.average('rating').to_f
-    paper.update_attribute( :average_rating, avg_rating)
-  end
+  belongs_to :paper
 end
